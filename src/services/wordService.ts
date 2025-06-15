78e6f1bd71234e9ed1f4da8f5b07ac8adafea816
Anna Maria Heliasz <annemaryheliasz@gmail.com>
2025-06-15 18:51:27 +0200

    Initial frontend routing and word list rendering

--- src/services/wordService.ts
+++ src/services/wordService.ts
@@ -1,7 +1,16 @@
 import { api } from './api';
 import type { Word } from '../types/word';
 
+type PaginatedResponse = {
+  total: number;
+  page: number;
+  limit: number;
+  totalPages: number;
+  results: Word[];
+};
+
 export const fetchSampleWords = async (): Promise<Word[]> => {
-  const res = await api.get<Word[]>('/');
-  return res.data;
+  const res = await api.get<PaginatedResponse>('/words');
+   console.log('📦 API response:', res.data);
+  return res.data.results; 
 };
