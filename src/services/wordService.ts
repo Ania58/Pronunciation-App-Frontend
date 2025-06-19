2c63ebde34b20cd71998683dbb56f5d3bae60abc
Anna Maria Heliasz <annemaryheliasz@gmail.com>
2025-06-19 23:26:07 +0200

    feat: add fetchAllWords function to support pagination, sorting, and search for full word list

--- src/services/wordService.ts
+++ src/services/wordService.ts
@@ -14,3 +14,16 @@ export const fetchFilteredWords = async (): Promise<Word[]> => {
    console.log('📦 API response:', res.data);
   return res.data.results; 
 };
+
+export const fetchAllWords = async (
+  page = 1,
+  limit = 50,
+  sort = 'word',
+  order = 'asc',
+  query = ''
+): Promise<PaginatedResponse> => {
+  const res = await api.get<PaginatedResponse>('/words/all', {
+    params: { page, limit, sort, order, query },
+  });
+  return res.data;
+};
