eef8035839a50a888f6bfdce65410ffe6b2ef330
Anna Maria Heliasz <annemaryheliasz@gmail.com>
2025-06-21 20:20:31 +0200

    Add MainPage component and set as default route

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import MainPage from './pages/MainPage';
 import WordList from './components/WordList';
 import WordDetails from './components/WordDetails';
 import WordBrowser from './components/WordBrowser';
@@ -7,7 +8,7 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<h1 className="text-2xl">Home</h1>} />
+        <Route path="/" element={<MainPage />} />
         <Route path="/words" element={<WordList />} />
         <Route path="/words/:id" element={<WordDetails />} />
         <Route path="/words-browser" element={<WordBrowser />} />
