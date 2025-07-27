f5986aba027982b71780bed7c872578a2c54c253
Anna Maria Heliasz <annemaryheliasz@gmail.com>
2025-07-27 14:56:39 +0200

    fix: scroll to top when RegisterPage loads

--- src/pages/RegisterPage.tsx
+++ src/pages/RegisterPage.tsx
@@ -1,5 +1,10 @@
 import Register from '../auth/Register';
+import { useEffect } from 'react';
 
 export default function RegisterPage() {
+   useEffect(() => {
+      window.scrollTo(0, 0);
+   }, []);
+
    return <Register />;
 }
