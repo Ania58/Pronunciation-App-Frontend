715f5cb3fb8dd1ebf8b842bf412cb6a01ace3f03
Anna Maria Heliasz <annemaryheliasz@gmail.com>
2025-07-27 14:49:13 +0200

    feat: add scroll-to-top behavior on LoginPage using window.scrollTo on mount

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,5 +1,9 @@
 import Login from '../auth/Login';
+import { useEffect } from 'react';
 
 export default function LoginPage() {
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
   return <Login />;
 }
